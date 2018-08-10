62a164666b39dd8a52229e41d875a0e082e339b7
Joel Herrick <joel.herrick@gmail.com>
2018-08-10 10:16:45 -0700

    added landlord/tenant/view-listings .js and respective handlebar files

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
-var express = require('express');
+// CS61 Project
 
+var express = require('express');
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+var mysql = require('./dbcon.js');
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 3000);
+app.set('mysql', mysql);
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/static', express.static('public'));
+app.use('/', express.static('public'));
+app.set('port', process.argv[2]);
 
-app.get('/',function(req,res){
+// Routes & functions files
+app.use('/view-listings', require('./view-listings.js'));
+app.use('/tenant', require('./tenant.js'));
+app.use('/landlord', require('./landlord.js'));
+
+
+
+
+// Home Route
+app.get('/', function(req, res){
   res.render('home');
 });
 
-app.get('/other-page',function(req,res){
-  res.render('other-page');
-});
 
-
-function genContext(){
-  var stuffToDisplay = {};
-  stuffToDisplay.time = (new Date(Date.now())).toLocaleTimeString('en-US');
-  return stuffToDisplay;
-}
-
-app.get('/time',function(req,res){
-  res.render('time', genContext());
-});
-
+// ERROR ROUTES
 app.use(function(req,res){
   res.status(404);
   res.render('404');
@@ -33,11 +36,12 @@ app.use(function(req,res){
 
 app.use(function(err, req, res, next){
   console.error(err.stack);
-  res.type('plain/text');
   res.status(500);
   res.render('500');
 });
 
+
+// Server listen config
 app.listen(app.get('port'), function(){
   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
